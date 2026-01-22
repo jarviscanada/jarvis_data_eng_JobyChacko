559d2a39735cd48c0712f5fcf84c9fb7f5cbc518
joby chacko <jobychacko2001@gmail.com>
2026-01-22 18:59:05 +0000

    add DDL for host information and usage data

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,33 @@
+\c host_agent;
+
+-- switch to host_agent database
+\c host_agent;
+
+-- create host_info table
+CREATE TABLE IF NOT EXISTS public.host_info (
+  id SERIAL NOT NULL,
+  hostname VARCHAR NOT NULL,
+  cpu_number INT2 NOT NULL,
+  cpu_architecture VARCHAR NOT NULL,
+  cpu_model VARCHAR NOT NULL,
+  cpu_mhz FLOAT8 NOT NULL,
+  l2_cache INT4 NOT NULL,
+  "timestamp" TIMESTAMP NOT NULL,
+  total_mem INT4 NOT NULL,
+  CONSTRAINT host_info_pk PRIMARY KEY (id),
+  CONSTRAINT host_info_un UNIQUE (hostname)
+);
+
+-- create host_usage table
+CREATE TABLE IF NOT EXISTS public.host_usage (
+  "timestamp" TIMESTAMP NOT NULL,
+  host_id INT4 NOT NULL,
+  memory_free INT4 NOT NULL,
+  cpu_idle INT2 NOT NULL,
+  cpu_kernel INT2 NOT NULL,
+  disk_io INT4 NOT NULL,
+  disk_available INT4 NOT NULL,
+  CONSTRAINT host_usage_host_info_fk
+    FOREIGN KEY (host_id)
+    REFERENCES public.host_info (id)
+);
