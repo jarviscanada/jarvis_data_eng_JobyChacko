52c5ccb7482eda9b451065e46e0fc129d911dab2
joby chacko <jobychacko2001@gmail.com>
2026-01-22 20:20:42 +0000

    update psql_docker

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -6,26 +6,21 @@ db_username=$2
 db_password=$3
 
 # Start docker
-# Make sure you understand the double pipe operator
 sudo systemctl status docker > /dev/null 2>&1 || sudo systemctl start docker
 
-# Check container status (try the following cmds on terminal)
+# Check container status
 docker container inspect jrvs-psql
 container_status=$?
 echo "DEBUG: cmd=$cmd"
 echo "DEBUG: container_status=$container_status"
 echo "DEBUG: args_count=$# user=$db_username pass=$db_password"
-# User switch case to handle create|stop|start opetions
 case $cmd in 
   create)
-  
-  # Check if the container is already created
   if [ $container_status -eq 0 ]; then
 		echo 'Container already exists'
 		exit 1	
 	fi
 
-  # Check # of CLI arguments
   if [ $# -ne 3 ]; then
     echo 'Create requires username and password'
     exit 1
@@ -36,17 +31,15 @@ case $cmd in
   # Start the container
   echo "Running docker run..."
 	docker run --name jrvs-psql -e POSTGRES_USER=$db_username -e POSTGRES_PASSWORD=$db_password  -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-  # Make sure you understand what's `$?`
 	exit $?
 	;;
 
   start|stop) 
-  # Check instance status; exit 1 if container has not been created
+
   if [ $container_status -ne 0 ]; then
     echo 'Container does not exist'
     exit 1
   fi
-  # ... todo
 
   # Start or stop the container
 	docker container $cmd jrvs-psql
